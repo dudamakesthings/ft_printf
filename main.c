fd5e6beb2f7085aae4d3f82c4f19f1e7b4bf9920
dudamakesthings <dudamakesthings@gmail.com>
2023-11-29 11:05:35 +0100

    Add main with some conditions

---
+++ main.c
@@ -0,0 +1,21 @@
+#include "ft_printf.h"
+#include <stdio.h>
+
+int main()
+{
+	// char *p = "string";
+	// ft_printf("%c%c%d%c\n", 'a', 'b', 3, '%');
+	// ft_printf("%c%c%d%s\n", 'a', 'b', 3, "bla");
+	// ft_printf("ble%c%c%i%sbla%%%x\n", 'a', 'b', 120, "foo", 5151);
+	// ft_printf("%%\n");
+	// ft_printf("ble%c%c%i%sbla%%%X\n", 'a', 'b', 120, "foo", 5151);
+	// printf("%%\n");
+	// printf("%d\n", printf("hello"));
+	// printf("%d\n", ft_printf("hello"));
+	// int  original = printf("%p\n %\0 ujruiturtiyut", &p);
+	// int my_len = ft_printf("%p\n %\0 ujruiturtiyut", &p);
+	// printf("%d, %d", original, my_len);
+	// int original = printf("%%%"); //someone fail me because this test, and there is a warning!
+	// int my_len = ft_printf("%%%");
+	// printf("this is original %d,this is my %d", original, my_len);
+}
